40185d3ce024cdfd8727838bdf8b4e775cd3c4d2
deBubz <bao.es.hoang2@gmail.com>
2020-01-24 15:09:02 +1100

    renameing

--- speedrun/py4e/assessed/03ex.py
+++ speedrun/py4e/assessed/03ex.py
@@ -1,35 +1,7 @@
-def overtime():
-    hrs = float(input("Enter Hours:"))
-    pay = float(input("Enter rate:"))
-    
-    
-    def paying(hours, pay):
-        return (hours * pay)
-    
-    if hrs > 40:
-        ot_pay = pay*1.5
-        ot_hrs = hrs - 40
-        total = paying(40, pay) + paying(ot_hrs, ot_pay)
-        print(total)
-    else:
-        print(paying(hrs, pay))
+# This first line is provided for you
+# # Enter hours and rate
+# # print gross earnings
 
-def grading():
-    sx = input("Enter Score: ")
-    score = float(sx)
-    msg = ''
-
-    if score <= 1.0 and score >= 0.9:
-    	msg = "A"
-    elif score >= 0.8:
-    	msg = "B"
-    elif score >= 0.7:
-    	msg = "C"
-    elif score >= 0.6:
-    	msg = "D"
-    elif score >= 0 and score < 0.6:
-    	msg = "F"
-    else:
-    	msg = "error"
-
-    print(msg)
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+print("Pay: %s" % (float(rate) * float(hrs)))
