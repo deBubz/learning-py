d109a019fb9af22339d7fbc7ab08b754a63a4c71
bubz <bao.es.hoang2@gmail.com>
2020-02-19 16:10:14 +1100

    07 file read bug

--- speedrun/py4e/notes/07-files/07file.py
+++ speedrun/py4e/notes/07-files/07file.py
@@ -1,37 +1,45 @@
+# NOTE: bug when trying to run e2 twice
+# Handler needs to be resetted
+# handler reached the end of file
+# therefore when ran again, it starts itterating from the end
+
 # global file select
-fname = None
 fh = None
 
-def open_file():
+def open_file(f_name):
+    if f_name == "notnow":
+        print("get bamboozled")
+        exit()
     try:
-        # string = "../data/" + input("File Nale: ")
-        string = "mbox-short.txt"
-        handler = open("mbox-short.txt")
-        print("Succesfuly oppened", string)
+        handler = open(f_name)
+        print("Successfully opened", f_name)
         return handler
     except:
-        print("Unable to read", string)
+        print("Unable to read", f_name)
+        print("Enter one of these:\nmbox.txt\nmbox-short.txt")
         exit()
-    
-
 
 # ex 1
 # read and prints file content in uppercase
 def e1_toupper():
-    line = 0
-    for l in fh:
-        if line == 10: break
-        print(l.upper())
-        line = line + 1
+    try:
+        limit = int(input("Uppercase how many lines "))
+        line = 0
 
-
+        for l in fh:
+            if line == limit: break
+            print(l.upper())
+            line = line + 1
+    except:
+        print("Please enter a number")
+        exit()
 
 # ex 2
 # Use the file name mbox-short.txt as the file name
-def ex2_counting_avg():
+def e2_counting_avg():
     total = float(0)
     count = 0
-
+    print(total, count)
     for line in fh:
         if not line.startswith("X-DSPAM-Confidence:"): continue
         else:
@@ -39,26 +47,11 @@ def ex2_counting_avg():
             total = total + float(line[20:])
 
     # do average
+    if count == 0:
+        print("Not present in file")
+        exit()
     print("Average spam confidence: %s" % (float(total)/ count))
     print()
-
-
-
-# ex 3
-# ask to input data name, add easter egg
-def e3_easter_egg():
-    file_name = input("Enter a file name: ")
-    if file_name == "this is lame":
-        print("BAMBOOZLED")
-        exit()
-
-    count = 0
-    for l in fh:
-        count = count + 1
-
-    print(count)
-
-
 
 ################################
 ##      Do the Program        ##
@@ -68,8 +61,7 @@ def run_prompt():
     print("07 files ex")
     print()
     print("1. string name")
-    print("2. gross pay calc")
-    print("3. random expressions")
+    print("2. avg of something")
     print("c. change selected file")
     print()
     return input("pick your choice: ")
@@ -85,12 +77,12 @@ def run():
         a = run_prompt()
         if a == 'x': close_prompt()
         elif a == '1': e1_toupper()
-        elif a == '2': ex2_counting_avg()
+        elif a == '2': e2_counting_avg()
         # elif a == '3': e4_expressions()
-        elif a == 'c': open_file()
+        elif a == 'c': open_file(input("Enter File Name: "))
         else:
             print("======================")
             print("Invalid input\n")
 
-fh = open_file() 
+fh = open_file("mbox-short.txt") 
 run()
