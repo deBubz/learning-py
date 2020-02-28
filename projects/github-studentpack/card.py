e3e5cd357b42a5c87f76c507ffe0365560654fd0
bubz <bao.es.hoang2@gmail.com>
2020-02-29 03:59:20 +1100

    its working! add readme

--- projects/github-studentpack/card.py
+++ projects/github-studentpack/card.py
@@ -2,4 +2,10 @@ class Card():
     def __init__(self, name):
         self.card_name = name
         self.card_desc = ''
-        self.card_bonus = list()
+        self.card_bonuses = list()
+    
+    def sh_detail(self) -> str:
+        msg = self.card_name + '\n'
+        for b in self.card_bonuses:
+            msg = msg + '\t' + b + '\n'
+        return msg
--- projects/github-studentpack/get-from-soup.py
+++ projects/github-studentpack/get-from-soup.py
@@ -1,25 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 from card import Card
+import re
 
 URL = 'https://education.github.com/pack'
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
-
-# find by id
-# results = soup.find(id='unity')
-
-# find cards
-elements = soup.find_all('div', class_='col-sm-6 col-lg-4 mb-3 mb-sm-5')
-for e in elements:
-    tag1 = e.dt.span
-    tag2 = e.dd.span
-    print(tag1)
-    print()
-    print(tag2)  
-    break
+card_list = list()
 
 # name              dt.span     class="d-none"
 # desc              dd.p        class="mb-3"
 # bonus list        findall     class="f5 text-gray"
+
+# find cards
+elements = soup.find_all('div', class_='col-sm-6 col-lg-4 mb-3 mb-sm-5')
+for e in elements:
+    # name
+    name = e.dt.span.string
+    card = Card(name)
+
+    # description
+    desc = e.find('p', class_='mb-3').string
+    card.card_desc = desc
+
+    # bonus
+    bonus_tag = e.find_all('p', class_='f5 text-gray')
+    for t in bonus_tag:
+        extract = re.search('</span> (.*)</p>', str(t)).group(1)
+        card.card_bonuses.append(extract)
+
+    card_list.append(card)
+
+for c in card_list:
+    print(c.sh_detail())
