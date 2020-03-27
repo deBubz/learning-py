c002fc70522469d735031f08444f8ba2082dad21
debubz <bao.es.hoang2@gmail.com>
2020-03-28 03:38:02 +1100

    updated handbookquery readme, cleaned up comments

--- projects/uts-hndbk-query-scraper/README.md
+++ projects/uts-hndbk-query-scraper/README.md
@@ -1,6 +1,6 @@
 # Github Education Pack
 
-A simple web scraper using `python` to get a list of tools offered in the [Githun Student Developer pack](https://education.github.com/pack)
+A simple web scraper using `python` to get the first result for a [UTS handbook query](https://handbook.uts.edu.au/)
 
 ## Requirements
 
@@ -12,6 +12,10 @@ pip install beautifulsoup4
 
 ## Run
 
+use the course number or name as the argument
+
 ```shell
-python3 get-from-soup.py
+python3 get-from-soup.py 31251
+
+python3 get-from-soup.py "data structures"
 ```
--- projects/uts-hndbk-query-scraper/get-from-soup.py
+++ projects/uts-hndbk-query-scraper/get-from-soup.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup 
-# from card import Card
 import re
 import sys
 
 search_term = ""
 string_arr = sys.argv[1].split()
 
+# parse query string
+# dont worry about this if just using number
 if (len(string_arr) > 1):
     for i in range(len(string_arr)):
         if (i) < len(string_arr) - 1:
@@ -16,41 +17,11 @@ if (len(string_arr) > 1):
 else:
     search_term = string_arr[0]
     
-
+# soup stuff
 URL = 'https://cfsites1.uts.edu.au/publications/search.cfm?scope=site&q=' + search_term
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# print(page.content)
-
-# # name              dt.span     class="d-none"
-# # desc              dd.p        class="mb-3"
-# # bonus list        findall     class="f5 text-gray"
-
+# done
 first_result = soup.find('span', class_='rs_title').getText()
 print(first_result)
-
-# # list of cards
-# elements = soup.find_all('div', class_='col-sm-6 col-lg-4 mb-3 mb-sm-5')
-# for e in elements:
-#     # name
-#     name = e.dt.span.string
-#     card = Card(name)
-
-#     # description
-#     desc = e.find('p', class_='mb-3').string
-#     card.card_desc = desc
-
-#     # bonus
-#     bonus_tag = e.find_all('p', class_='f5 text-gray')
-#     for t in bonus_tag:
-#         extract = re.search('</span> (.*)</p>', str(t)).group(1)
-#         card.card_bonuses.append(extract)
-
-#     card_list.append(card)
-#     count = count + 1
-
-# # print
-# for c in card_list:
-#     print(c.to_string())
-# print(count, 'avaliable packages')
