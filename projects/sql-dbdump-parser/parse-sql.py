9c43d564a3d072f4b98718d9415c945cdd918872
bubz <bao.es.hoang2@gmail.com>
2020-04-13 03:07:56 +1000

    cleaned up parse-sql.py abit

--- projects/sql-dbdump-parser/parse-sql.py
+++ projects/sql-dbdump-parser/parse-sql.py
@@ -1,18 +1,24 @@
 import re
 
-output = open('datadict.csv', 'w')
+tbl_count = 0
+file_name = "datadict.csv"
+output = open(file_name, 'w')
     
-with open('./db_baseSchema.sql') as f:
-    for line in f:
-        l = line.strip()
-        if re.match('^CREATE', l):          # db name
-            sent = l.split(' ')[2]
-            db_name = sent[1:len(sent) - 1]
-            output.write(db_name + '\n')
-        elif re.match('^\`',l):             # db attr
-            sent = l[:len(l) - 1]
-            attrs = ','.join(sent.split(' '))
-            output.write(attrs + '\n')
-        elif re.match('^PR', l):            # pk
-            pk_list = l[13:len(l) - 1]
-            output.write(pk_list + '\n\n')
+
+for line in open('./db_baseSchema.sql'):
+    l = line.strip()
+    if re.match('^CREATE', l):          # db name
+        tbl_count = tbl_count + 1
+        sent = l.split(' ')[2]
+        db_name = sent[1:len(sent) - 1]
+        output.write(db_name + '\n')
+    elif re.match('^\`',l):             # db attr
+        sent = l[:len(l) - 1]
+        attrs = ','.join(sent.split(' '))
+        output.write(attrs + '\n')
+    elif re.match('^PR', l):            # pk
+        pk_list = l[13:len(l) - 1]
+        output.write(pk_list + '\n\n')
+
+print("saved to " + file_name)
+print("There are " + str(tbl_count) + " tables.")
